c7439c147eba4a95edc3926802d2bfaec4455238
Mike Murray <mikemurray056@gmail.com>
2021-12-30 15:43:14 -0500

    Added additional gameobjects in menu to represent empty space

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,10 +18,17 @@ public class GameManager : MonoBehaviour
         //icon object set up. Each one is instantiated and given random scriptable object data
         iconObjects = new GameObject[MaxIconObjects];
         Vector3 iconPos = Vector3.zero;
+        SpriteRenderer menuSr = inventory.GetComponent<SpriteRenderer>();   //need this to position the icons
+        float xOffset = 1.5f;
+        float yOffset = -1f;
+        float xBounds = menuSr.bounds.min.x;
+        float yBounds = menuSr.bounds.max.y;
+
+        Random.InitState(1001);
 
         for (int i = 0; i < iconObjects.Length; i++)
         {
-            iconObjects[i] = Instantiate(iconPrefab, iconPos, Quaternion.identity);
+            iconObjects[i] = Instantiate(iconPrefab);
 
             //get random scriptable object data
             int randIcon = Random.Range(0, im.icons.Length);
@@ -33,8 +40,32 @@ public class GameManager : MonoBehaviour
             tm.text = im.icons[randIcon].iconName;
 
             //TODO: place the icon objects into a random position in the inventory
-            iconPos = new Vector3(iconPos.x + 1, iconPos.y, iconPos.z);
+            //iconPos = new Vector3(iconPos.x + 1, iconPos.y, iconPos.z);
+            int randRow = Random.Range(0, inventory.MaxRows);
+            int randCol = Random.Range(0, inventory.MaxCols);
+            inventory.inventorySpace[randRow, randCol] = iconObjects[i];
+
+            iconObjects[i].transform.position = new Vector3(xBounds + (float)randCol + xOffset, yBounds - (float)randRow + yOffset, iconObjects[i].transform.position.z);
         }
+
+        /*******displays item position in console.*****/
+        string menuString = "Icon Locations (A = icon)\n";
+        for (int i = 0; i < inventory.MaxRows; i++)
+        {
+            menuString += "[";
+            for (int j = 0; j < inventory.MaxCols; j++)
+            {
+                if (inventory.inventorySpace[i,j] == null)
+                    menuString += "0 ";
+                else
+                    menuString += "A ";
+            }
+
+            menuString += "]\n";
+        }
+
+        Debug.Log(menuString);
+        /******************************/
     }
 
     // Update is called once per frame
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Menu : MonoBehaviour
 {
     public GameObject[,] inventorySpace;
-    int MaxRows {get;} = 3;
-    int MaxCols {get;} = 6;
+    public int MaxRows {get;} = 3;
+    public int MaxCols {get;} = 6;
+
+    void Awake()
+    {
+        //menu starts empty. If I can't get around the errors that will result from empty indexes, I can try inserting an empty gameobject.
+        inventorySpace = new GameObject[MaxRows, MaxCols];
+
+    }
     // Start is called before the first frame update
     void Start()
     {
         //menu starts empty. If I can't get around the errors that will result from empty indexes, I can try inserting an empty gameobject.
-        inventorySpace = new GameObject[MaxRows, MaxCols];
+        //inventorySpace = new GameObject[MaxRows, MaxCols];
 
         
 
