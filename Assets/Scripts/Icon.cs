4b245a26bb3fb1dcb25a549e1fc66bb5986ad42f
Mike Murray <mikemurray056@gmail.com>
2021-12-30 12:18:25 -0500

    Using scriptable objects for the icon data; can now add random objects
    I went with scriptable objects to keep the game as small as possible, and there isn't a lot of information required for the icons.

---
+++ Assets/Scripts/Icon.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Icon")]
+public class Icon : ScriptableObject
+{
+    public string iconName;
+    public Sprite iconImage;
+}
---
+++ Assets/Scripts/IconManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/* This script will create 5 icon game objects at runtime using a single prefab. The icons are scriptable objects. */
+public class IconManager : MonoBehaviour
+{
+    int MaxIconObjects {get;} = 5;
+    public Icon[] icons;     //55 total
+    public GameObject[] iconObjects;
+    public GameObject iconPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //icon object set up. Each one is instantiated and given random scriptable object data
+        iconObjects = new GameObject[MaxIconObjects];
+        Vector3 iconPos = Vector3.zero;
+
+        for (int i = 0; i < iconObjects.Length; i++)
+        {
+            iconObjects[i] = Instantiate(iconPrefab, iconPos, Quaternion.identity);
+
+            //get random scriptable object data
+            int randIcon = Random.Range(0, icons.Length);
+            SpriteRenderer sr = iconObjects[i].GetComponent<SpriteRenderer>();
+            sr.sprite = icons[randIcon].iconImage;
+
+            TextMeshProUGUI tm = iconObjects[i].GetComponentInChildren<TextMeshProUGUI>();
+            tm.text = icons[randIcon].iconName;
+
+            iconPos = new Vector3(iconPos.x + 1, iconPos.y, iconPos.z);
+        }
+        //SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        //sr.sprite = icon[0].iconImage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
