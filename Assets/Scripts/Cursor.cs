d144d4901ac01dc84ee4d58b6588d63878c266e7
Mike Murray <mikemurray056@gmail.com>
2022-01-03 21:09:19 -0500

    added JSON to debug Unity through VSCode; modified blink timing for cursor coroutine
    Also, I tried to solve the issue with the AnimateText coroutine, where the text doesn't fully clear when it's interrupted. No luck.

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -196,7 +196,7 @@ public class Cursor : MonoBehaviour
     IEnumerator Blink()
     {
         cursorSr.enabled = !cursorSr.enabled;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.08f);
         coroutineRunning = false;
     }
 }
