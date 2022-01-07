f526232cc103d456f9a2bf65822cffa1e3bb3d14
Mike Murray <mikemurray056@gmail.com>
2022-01-07 16:47:28 -0500

    A bit of cleanup to make the background scroll code more understandable

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -6,20 +6,20 @@ public class BackgroundScroller : MonoBehaviour
     public GameObject[] backgrounds;
     float scrollSpeed;
     public GameManager gm = GameManager.instance;
-    Vector3 screenPos;          //used to get screen coordinates to check boundaries based on position of game manager.
+    //Vector3 screenPos;          //used to get screen coordinates to check boundaries based on position of game manager.
     SpriteRenderer bgSr;
 
-    float screenBoundary;       //number of units to reach one edge of the screen. Pixels per unit / 2.
+    float screenBoundary;       //number of units to reach one edge of the screen from the origin.
     float xOffset;              //used to cover any small gaps between backgrounds.
 
     // Start is called before the first frame update
     void Start()
     {
         scrollSpeed = 1;
-        screenPos = Camera.main.WorldToViewportPoint(gm.transform.position);
+        //screenPos = Camera.main.WorldToViewportPoint(gm.transform.position);
         bgSr = backgrounds[0].GetComponent<SpriteRenderer>();   //both backgrounds are same, doesn't matter which one is referenced.
-        screenBoundary = -8.88f;                                   //want to check the left boundary
-        Debug.Log("Screen Pos " + screenPos.x);
+        screenBoundary = bgSr.GetComponent<SpriteRenderer>().bounds.extents.x;
+        //Debug.Log("Screen Pos " + screenPos.x);
         Debug.Log("screenBoundary " + screenBoundary);
         xOffset = 0.08f;
     }
@@ -32,10 +32,10 @@ public class BackgroundScroller : MonoBehaviour
             background.transform.position = new Vector3(background.transform.position.x - scrollSpeed * Time.deltaTime, background.transform.position.y,
                 background.transform.position.z);
             
-            //if a background is out of bounds, move it
-            if (background.transform.position.x + bgSr.GetComponent<SpriteRenderer>().bounds.extents.x < screenBoundary)
+            //if a background is out of bounds, move it to other side of screen
+            if (background.transform.position.x + bgSr.GetComponent<SpriteRenderer>().bounds.extents.x < -screenBoundary)   //checking left screen boundary
             {
-                background.transform.position = new Vector3(bgSr.GetComponent<SpriteRenderer>().bounds.extents.x * 2 - xOffset, background.transform.position.y,
+                background.transform.position = new Vector3(screenBoundary * 2 - xOffset, background.transform.position.y,
                     background.transform.position.z);
             }
         }
